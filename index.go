cafd001df4b8ee1bdb0296a77458529747b957bf
alsaa19 <99720129+altsaqif@users.noreply.github.com>
2022-02-16 02:03:54 +0700

    Update

--- index.go
+++ index.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("halo")
+	
+	Philips := make(map[string]string)
+	Philips["Nama"] = "Philips"
+	Philips["Alamat"] = "Medan"
+	fmt.Println(Philips)
+	fmt.Println(Philips["Nama"])
+	fmt.Println(Philips["Alamat"])
 }
