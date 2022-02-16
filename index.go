59ee5f1afa7646d92c5a85e9fabd3237a3b0e54c
alsaa19 <99720129+altsaqif@users.noreply.github.com>
2022-02-16 12:02:10 +0700

    Added slice data type

--- index.go
+++ index.go
@@ -4,4 +4,28 @@ import "fmt"
 
 func main() {
 	fmt.Println("halo")
+	
+	
+// Type Data Slice
+var aray = [...]string{"Al Tsaqif", "Deri", "Ucup", "Nadie", "Azka"}
+
+	var slice = aray[2:4]
+	fmt.Println(slice)
+	fmt.Println(slice[0])
+	fmt.Println(len(slice))
+	fmt.Println(cap(slice))
+
+	aray[0] = "Asep"
+	fmt.Println(aray)
+
+	slice[0] = "Agus"
+	fmt.Println(slice)
+
+	var apent = append(slice, "Zanan")
+	apent[0] = "Philips"
+	fmt.Println(apent)
+	fmt.Println(len(apent))
+	fmt.Println(cap(apent))
+	fmt.Println(slice)
+	fmt.Println(aray)
 }
